7ecd5581dea3f60c4bc20e463c98636c074a8cf2
TheBongCoder <thebongcoder@tutamail.com>
2023-10-21 23:44:13 +0530

    swagger changes

--- src/main/java/com/thebongcoder/blog/blogapp/config/SwaggerConfig.java
+++ src/main/java/com/thebongcoder/blog/blogapp/config/SwaggerConfig.java
@@ -6,21 +6,36 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.thebongcoder.blog.blogapp.utils.AppConstants.AUTHORIZATION_HEADER;
 
 @Configuration
 @EnableWebMvc
-public class SwaggerConfig  {
+public class SwaggerConfig {
+
+
+    private ApiKey authorizationKey() {
+        return new ApiKey("Authorization", AUTHORIZATION_HEADER, "header");
+    }
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(authorizationKey()))
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.thebongcoder.blog.blogapp"))
                 .paths(PathSelectors.regex("/.*"))
                 .build().apiInfo(apiInfoMetaData());
     }
+
     private ApiInfo apiInfoMetaData() {
         return new ApiInfoBuilder().title("Blog-App")
                 .description("Blog web app")
@@ -31,4 +46,14 @@ public class SwaggerConfig  {
                 .build();
     }
 
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
 }
--- src/main/java/com/thebongcoder/blog/blogapp/utils/AppConstants.java
+++ src/main/java/com/thebongcoder/blog/blogapp/utils/AppConstants.java
@@ -2,14 +2,16 @@ package com.thebongcoder.blog.blogapp.utils;
 
 public class AppConstants {
 
-    public static final String DEFAULT_PAGE_NO="0";
-    public static final String DEFAULT_PAGE_SIZE="5";
-    public static final String DEFAULT_PAGE_SORT_BY="id";
-    public static final String DEFAULT_PAGE_SORT_DIR="asc";
+    public static final String DEFAULT_PAGE_NO = "0";
+    public static final String DEFAULT_PAGE_SIZE = "5";
+    public static final String DEFAULT_PAGE_SORT_BY = "id";
+    public static final String DEFAULT_PAGE_SORT_DIR = "asc";
 
-    public static final String PAGE_NO="pageNo";
-    public static final String PAGE_SIZE="pageSize";
-    public static final String PAGE_SORT_BY="sortBy";
-    public static final String PAGE_SORT_DIR="sortDir";
-    public static final String POST_ID="postId";
+    public static final String PAGE_NO = "pageNo";
+    public static final String PAGE_SIZE = "pageSize";
+    public static final String PAGE_SORT_BY = "sortBy";
+    public static final String PAGE_SORT_DIR = "sortDir";
+    public static final String POST_ID = "postId";
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
 }
